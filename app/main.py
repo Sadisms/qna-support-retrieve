257a571d133a1a0fc479e36c543939ea32275337
Дмитрий <madara@deeplay.io>
2025-08-25 19:49:49 +0600

    Refactor FastAPI application initialization and remove database dependency
    - Eliminated the lifespan context manager from main.py, simplifying the application startup process by removing database-related setup.
    - Cleaned up imports for improved code clarity and maintainability.

--- app/main.py
+++ app/main.py
@@ -1,30 +1,21 @@
 from fastapi import FastAPI, HTTPException
-from contextlib import asynccontextmanager
 
 from app.core.config import get_config
-from app.core.database import engine
 from app.core.exceptions import QnAException
 from app.core.error_handlers import (
     qna_exception_handler,
     http_exception_handler,
     general_exception_handler
 )
-from app.models.database import BaseModel
 from app.api.qa_routes import router as qa_router
 from app.api.health_routes import router as health_router
 
-
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    BaseModel.metadata.create_all(bind=engine)
-    yield
 
 
 config = get_config()
 
 app = FastAPI(
     title="QnA-Support-Retriever",
-    lifespan=lifespan
 )
 
 app.add_exception_handler(QnAException, qna_exception_handler)
